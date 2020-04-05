c1685d9ee11cacade33516f52b7766897f70a7a4
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2020-04-05 13:21:55 +0600

    Create LightOj1045.cpp

---
+++ LightOj1045.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dd double
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define mx 1000005
+#define mod 1000000007
+#define fr first
+#define cti(a) (a-48)
+#define itc(a) char(a+48)
+#define se second
+#define End cout<<"\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define memo(Array,val) memset(Array, val, sizeof Array)
+#define PI acos(-1)
+bool check(ll n, ll pos){ return (n&(1<<pos)); }
+bool Set(ll n, ll pos) {  return (n | (1<<pos)); }
+ld LOG(ld b, ld e){ return log(b)/log(e); }
+
+int tc=1;
+ld a[mx];
+
+void calc()
+{
+	a[0]=0;
+	for(int i=1; i<=mx-5; i++){
+		a[i]=a[i-1] + LOG(i,10);
+	}
+}
+
+void solve(int kk)
+{
+	ll n,base;
+	scanf("%lld %lld", &n, &base);
+	printf("Case %d: ", kk);
+	ll cnt = (a[n]/LOG(base,10))+1;
+	printf("%lld\n", cnt);
+}
+
+int main()
+{
+   calc();	
+   //fast;
+   int kk=0;
+   scanf("%d", &tc);
+   while(++kk<=tc) solve(kk);
+
+   return 0;
+}

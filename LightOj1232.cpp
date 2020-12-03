f5e301de0fa11117d19c14d03af18badff95b515
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2020-12-03 23:08:21 +0600

    Update LightOj1232.cpp

--- LightOj1232.cpp
+++ LightOj1232.cpp
@@ -1,48 +1,53 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
-#define dd double
-#define pb push_back
-#define pf push_front
-#define popb pop_back
-#define popf pop_front
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define mx 200005
+#define int long long
 #define mod 100000007
-#define fr first
-#define cti(a) (a-48)
-#define itc(a) char(a+48)
-#define se second
-#define End cout<<"\n"
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
-#define memo(Array,val) memset(Array, val, sizeof Array)
-#define check(n, pos) (n & (1<<pos))
-#define Set(n, pos) (n | (1<<pos))
-#define PI acos(-1)
+const int inf = 1e18;
+int ii, dp[105][10005];
 
-ll dp[10004], a[10004], n;
-
-ll func(ll k)
+int add(int &a, int &b)
 {
-    dp[0]=1;
-    for(ll i=0; i<n; i++){
-        for(ll j=a[i]; j<=k; j++) dp[j] = (dp[j]%mod+dp[j-a[i]]%mod)%mod;
+    a+=b;
+    if(a>=mod)
+        a-=mod;
+    return a;
+}
+
+signed main(){
+    //freopen("in.txt", "r", stdin);
+	int n, lim, t;
+	cin >> t;
+
+	while(t--){
+        cin >> n >> lim;
+
+	int v[n+10];
+
+	for(int i=1; i<=n; i++)
+        cin >> v[i];
+    
+    memset(dp, 0, sizeof dp);
+   
+        dp[n+1][0]=1;  ///Base case
+        dp[n+1][lim] = 0;  ///Base case
+    
+
+    for(int i=n; i>=1; i--){
+        for(int s=0; s<=lim; s++){
+            dp[i][s] = 0; /// Base Case
+            int op1=0, op2=0;
+
+            op1 = dp[i+1][s];
+            if((s-v[i])>=0){
+                op2 = dp[i][s-v[i]];
+            }
+            dp[i][s] = add(op1, op2);
+        }
     }
-    return dp[k];
-}
-void solve(ll e)
-{
-    ll k;
-    memo(dp,0);
-    scanf("%lld %lld", &n, &k);
-    for(ll i=0; i<n; i++) scanf("%lld", a+i);
-    printf("Case %lld: %lld\n", e, func(k));
-}
-int main()
-{
-   ll t,c=0;
-   scanf("%lld", &t);
-   while(++c<=t) solve(c);
-   return 0;
+
+
+    cout << "Case " << ++ii << ": " << dp[1][lim] << "\n";
+
+	}
+	return 0;
 }
